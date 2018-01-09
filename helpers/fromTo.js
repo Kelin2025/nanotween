c0c729fc239e81583b24f9b593fee29badbe855d
Kelin2025 <kelin2025@yandex.ru>
2018-01-09 05:04:43 +0300

    Reworked fromTo. Now it accepts custom callback

--- helpers/fromTo.js
+++ helpers/fromTo.js
@@ -1,17 +1,16 @@
-export default function fromTo(from, to) {
+function def(from, to, k) {
+  return from + (to - from) * k
+}
+
+export default function fromTo(from, to, cb) {
   return function(instance) {
     var fn = function(k) {
-      switch (typeof from) {
-        case 'number':
-          return from + (to - from) * k
-        case 'object':
-          return Object.keys.reduce(function(acc, key) {
-            acc[key] = fromTo(from[key], to[key])
-          }, {})
-        default:
-          return from
-      }
+      return typeof from === 'object'
+        ? Object.keys(from).reduce(function(acc, key) {
+          acc[key] = fromTo(from[key], to[key])
+        }, {})
+        : (cb || def)(from, to, k)
     }
-    instance.enhance(fn)
+    instance.convert(fn)
   }
 }
